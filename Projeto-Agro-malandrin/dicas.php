0fcdda890dc797f7052525dce20bf4af7e2f59a4
YuriLima1 <yurialeandresilvalima@gmail.com>
2024-08-28 14:07:56 -0300

    up 28/08

---
+++ Projeto-Agro-malandrin/dicas.php
@@ -0,0 +1,55 @@
+<?php
+    include_once 'includes/header.php'
+?>
+<header>
+<title>Dicas - Agro Malandrin</title>
+</header>
+
+<body id="dicas">
+    <div id="conteiner">
+        <table cellspacing="0" id="tableDicas1">
+            <th id="titulo">Dicas Para Seu Pet</th>
+
+            <tr>
+                <td>
+                    <ul>
+                        <li><strong>Alimentação Balanceada:</strong> Escolha alimentos de qualidade e adequados à espécie e porte do seu pet;</li><br>
+
+                        <li><strong>Atividade Física Regular:</strong> Proporcione exercícios diários para manter a saúde física e mental do animal;</li><br>
+
+                        <li><strong>Visitas Regulares ao Veterinário:</strong> Mantenha check-ups periódicos para prevenir e detectar problemas de saúde precocemente;</li><br>
+
+                        <li><strong>Higiene e Cuidados:</strong> Cuide da limpeza do ambiente e do pet, incluindo banhos, escovação e limpeza dos dentes;</li><br>
+
+                        <li><strong>Atenção Emocional:</strong> Dedique tempo para interação e carinho, pois isso é essencial para o bem-estar emocional do seu pet.</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        
+        <table cellspacing="0" id="tableDicas2">
+            <th id="titulo">Dicas Para Seu Pet</th>
+
+            <tr>
+                <td>
+                    <ul>
+                        <li><strong>Alimentação Balanceada:</strong> Escolha alimentos de qualidade e adequados à espécie e porte do seu pet;</li><br>
+
+                        <li><strong>Atividade Física Regular:</strong> Proporcione exercícios diários para manter a saúde física e mental do animal;</li><br>
+
+                        <li><strong>Visitas Regulares ao Veterinário:</strong> Mantenha check-ups periódicos para prevenir e detectar problemas de saúde precocemente;</li><br>
+
+                        <li><strong>Higiene e Cuidados:</strong> Cuide da limpeza do ambiente e do pet, incluindo banhos, escovação e limpeza dos dentes;</li><br>
+
+                        <li><strong>Atenção Emocional:</strong> Dedique tempo para interação e carinho, pois isso é essencial para o bem-estar emocional do seu pet.</li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+<?php
+include_once 'includes/footer.php';
+?>
+
---
+++ Projeto-Agro-malandrin/index.php
@@ -0,0 +1,39 @@
+<?php
+include_once 'includes/header.php'
+?>
+
+<header>
+<title>Agro Malandrin</title>
+</header>
+
+<main id="home">
+    <img src="multimidia/Logo_Home.png" alt="logo da agro malandrin" id="logo">
+
+    <p id="apresentacao">Somos uma empresa com mais de 12 anos de atuação na cidade de Itapira no Interior de São Paulo, 
+        Nosso ramo de atuação é a venda e distribuição de rações para diversos tipos de animais, como 
+        equinos, suinos, ovinos, bovinos e aves. <br> <br>
+        
+        Também vendemos equipamentos de serviço rural, utensílios de jardinagem, materiais pra piscina, 
+        telas de galinheiro e muito mais.<br> <br>
+    
+        Muito antes de termos essa agro, a mais ou menos 28 anos atrás, o dono José Francisco Malandrin, 
+        possuia um barracão para realizar seu comércio, vendendo grãos como feijão, arroz, etc.<br> <br>
+    
+        Os anos foram se passando e ele construíu uma grande agro, cujo o sobrenome da família será lembrado 
+        como marca registrada no nome da agro. 
+    </p>
+
+    <div id="foto1">
+        <div id="foto"> <img src="multimidia/faixada_AG.png" alt="foto da frente da loja" id="faixada">
+        <p>Rua Joaquim de Souza Dias - São Vincente, Itapira-SP</p></div>
+        
+        <div id="texto"><p>Nossa agro ja está <br> em funcionamento <br> a 12 anos! visite-nos!</p></div>
+    </div>
+
+
+
+</main>
+
+<?php
+include_once 'includes/footer.php';
+?>
