5b4b004576302a0a561889eb029488523002fc3c
YuriLima1 <yurialexandresilvalima@gmail.com>
2024-08-28 01:23:26 -0300

    projeto integrador 28/08

---
+++ Projeto-Agro-malandrin/includes/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- conectando com o css-->
+    <link href="estilos/estilo.css" rel="stylesheet" type="text/css">
+    
+    <!-- colocando o icone da pagina-->
+    <link rel="shortcut icon" href="multimidia/icones/planta.png" type="image/x-icon">
+
+</head>
+ 
+<body>
+    <header><!-- criando o header -->
+         <div><img src="multimidia/logo_Head.png" alt="logo Agro malandrin" id="logo"> </div>
+
+        <!-- criando o nav -->
+        <div id="navbar" >
+    
+            <!-- links do nav -->
+            <a href="index.php" id="nav"> <h1>Home</h1> </a>
+            <a href="Produtos.php" id="nav"> <h1>Produtos</h1> </a>  
+            <a href="dicas.php" id="nav"> <h1>Dicas</h1> </a>  
+
+        </div>
+    
+        <!-- colocando o botão para poder logar -->
+       <div id="login"> <a href="login.php" id="link"> <p>Ola, logue ou <br>cadastre-se</p> </a> </div>
+
+      
+    </header>
+    
+   
